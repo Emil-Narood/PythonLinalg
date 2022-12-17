300a046456f35b8666b61c52136cfba19f83d350
79202 <natkondra@yandex.ru>
2022-12-17 20:56:26 +0300

    17.11.2022

--- src/lib/matr3D.py
+++ src/lib/matr3D.py
@@ -33,3 +33,25 @@ def subtr_matr_matr(matr1, matr2):
         for j in range(len(arr1[i])):
             matr[i][j] = matr1[i][j] - matr2[i][j]
     return matr
+
+def matr3DDet(matr):
+    Det = matr[0][0] * matr[1][1] * matr[2][2] + \
+    matr[0][1] * matr[1][2] * matr[2][0] + \
+    matr[1][0] * matr[0][2] * matr[2][1] - \
+    matr[2][0] * matr[1][1] * matr[0][2] - \
+    matr[1][0] * matr[0][1] * matr[2][2] - \
+    matr[2][1] * matr[0][0] * matr[1][2]
+    return Det
+
+
+def matr3Dtransp(matr):
+    a21 = matr[1][0]
+    a31 = matr[2][0]
+    a32 = matr[2][1]
+    matr[1][0] = matr[1][2]
+    matr[2][0] = matr[0][2]
+    matr[2][1] = matr[0][1]
+    matr[1][2] = a21
+    matr[0][2] = a31
+    matr[0][1] = a32
+    return matr
